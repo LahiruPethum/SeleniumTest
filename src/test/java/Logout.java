e22e9ada621bc83f375d5418696e3c04d314dd3f
Lahiru Pethum <pethum013@gmail.com>
2023-12-16 05:56:44 +0530

    added new logout test case

--- src/test/java/Logout.java
+++ src/test/java/Logout.java
@@ -1,9 +1,14 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class Logout {
     @Test
@@ -28,4 +33,43 @@ public class Logout {
         driver.close();
     }
 
+
+    @Test
+    public void testcase2() throws InterruptedException  {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://www.singersl.com/");
+            driver.manage().window().maximize();
+
+            // Click on the login link
+            WebElement loginLink = driver.findElement(By.xpath("//*[@id=\"block-singer-account-menu\"]/ul/li[2]/a"));
+            loginLink.click();
+
+            // Enter login credentials
+            driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("pethum013@gmail.com");
+            driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123456789");
+            driver.findElement(By.xpath("//*[@id=\"login-submit\"]")).click();
+
+
+           driver.findElement(By.xpath("//*[@id=\"block-singer-account-menu\"]/ul/li[2]/a")).click();
+            WebElement logoutLink = driver.findElement(By.xpath("//*[@id=\"account-profile\"]/div[1]/div[2]/ul/li[7]/a"));
+
+            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", logoutLink);
+
+
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", logoutLink);
+
+
+            Thread.sleep(5000);
+
+            String expectedUrl = "https://www.singersl.com/";
+            String actualUrl = driver.getCurrentUrl();
+            Assert.assertEquals(actualUrl, expectedUrl);
+
+        } finally {
+            driver.quit();
+        }
+    }
 }
